8ccfad06dbbbb7bb92bd0f802b31dbaca4ea9ba7
Michal <m.matera09@wp.pl>
2019-03-31 14:19:58 +0200

    add readme and make game in one window

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# ElectronProject
+# Memory Game
+This project contains a simple card game.
+The goal of the game is to try to remember
+location of different cards and find all pairs as soon as possible.
+After reversing the first card, the time starts.
+ When the player
+turn face up
+two identical cards forming a pair, face up.
+When a player discovers that he wins all pairs of cards, he wins.
+After winning the name and time of the user is saved in the list of records. if you do not provide a name, the user is the default
+
+## Main screen
+![game](screenshots/mainPage.PNG)
+
+ ## Game screen
+ 
+ At the begining of the game, all cards are face down
+  
+![game](screenshots/gamePage.PNG)
+
+ The player puts successively  cards face up.
+
+![game](screenshots/gamePage3.PNG)
+
+When a player win a game his record would be save in Record List.
+
+![game](screenshots/gamePage4.PNG)
+
+## Record List screen
+
+Here is a Record List.
+
+![game](screenshots/recordList.PNG)
+
+## Instalation
+
+Clone the repository to your directory. Open command line  in this folder and write 
+````bash
+npm install 
+npm start
+````
+Enjoy the game.
--- main.js
+++ main.js
@@ -1,122 +1,121 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow , ipcMain} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-let recordWindow
-let gameWindow
+let mainWindow;
+let recordWindow;
+let gameWindow;
 
 
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 940,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    gameWindow = recordWindow = mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 940,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-    gameWindow = recordWindow = mainWindow
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html');
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        app.quit()
+    })
 }
 
 function createGameWindow() {
 
-    gameWindow = new BrowserWindow({
-        width: 1200,
-        height: 940,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+    // gameWindow = new BrowserWindow({
+    //     width: 1200,
+    //     height: 940,
+    //     webPreferences: {
+    //         nodeIntegration: true
+    //     }
+    // });
 
 
- gameWindow.loadFile('game.html')
+    mainWindow.loadFile('game.html');
 
 
-    gameWindow.on('closed', function () {
+    mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        gameWindow = null
+        app.quit()
+
     })
 }
-function createRecordListWindow(){
-    recordWindow = new BrowserWindow({
-        width: 1200,
-        height: 940,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+
+function createRecordListWindow() {
 
 
- recordWindow.loadFile('recordList.html')
+    mainWindow.loadFile('recordList.html');
 
 
-    recordWindow.on('closed', function () {
+    mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        recordWindow = null
+
+
+        app.quit()
     })
 }
-
 
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
 
-ipcMain.on('btn:exit',function () {
+ipcMain.on('btn:exit', function () {
     app.quit();
-})
+});
+ipcMain.on('btn:exit:menu', function () {
 
-ipcMain.on('btn:list',function () {
+    mainWindow.loadFile('index.html');
+});
+
+
+
+ipcMain.on('btn:list', function () {
     createRecordListWindow();
-})
+});
 
-ipcMain.on('btn:start',function () {
+ipcMain.on('btn:start', function () {
     createGameWindow();
-})
+});
 
-ipcMain.on('save:score',function () {
+ipcMain.on('save:score', function () {
 
-
-
-})
+});
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
